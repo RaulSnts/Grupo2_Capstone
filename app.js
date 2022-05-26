3c5cca41f9d6b556598d64c7420dec9bedaaa4ae
RaulSnts <raulsr64@gmail.com>
2022-05-27 01:01:22 +0200

    Session

--- app.js
+++ app.js
@@ -6,12 +6,18 @@ const bodyParser = require("body-parser");
 const session = require("express-session");
 const mongoose = require("mongoose");
 const Usuario = require("./models/Usuarios")
+const alert = require("alert");
 
 const { Db } = require("mongodb");
 const { userInfo } = require("os");
 
 
-var inicioSesionIncorrecto = false;
+var contrasenasDiferentes = false;
+var emailYaRegistrado = false;
+var datosIncompletos = false;
+
+var emailIncorrecto = false;
+var contraseñaIncorrecta = false;
 
 const password = "MOl48FssxblU2L6b";
 const uri = `mongodb+srv://raul_admin:${password}@cluster0.pjv02.mongodb.net/?retryWrites=true&w=majority`;
@@ -33,7 +39,7 @@ app.set('view options', { layout: 'layaout' });
 app.use(session({
   secret: "secret8465161616545610raul165513165clave",
   saveUninitialized: true,
-  resave: true,
+  resave: false,
 }));
 
 
@@ -47,18 +53,38 @@ app.get("/", (req, res, next) => {
 
 
 // Validación de credenciales de inicio de sesión
-app.post("/login", function (req, res) {
+app.post("/login", async (req, res) => {
   let email = req.body.email;
-  let pass = req.body.password;
-  let data = { inicioIncorrecto: true };
+  let password = req.body.password;
 
-  
 
-  if (email === 'a' /*consulta MONGODB USUARIOS*/ ) {
-    //SI ES CORRECTO COMPROBAR ESA CONTRASEÑA PARA ESE USUARIO
-    res.render("index");
-  } else {
-    res.render("index", data);
+  if((email.length <= 0) || (password.length <= 0)){
+    datosIncompletos = true;
+    res.render("index", { datosIncompletos, layout: false });
+  }else{
+
+    const checkEmail = await Usuario.findOne({ correo_electronico: email});
+
+    if(checkEmail){
+      const checkPassword = await Usuario.findOne({ correo_electronico: email, password: password});
+
+      if(checkPassword){
+        //////
+        let nombreUsuario = await Usuario.find( { correo_electronico: email }, "nombre" );
+        //console.log(nombreUsuario);
+        req.session.email = email;
+        req.session.save(function (err) {
+          res.render("home", { nombreUsuario });
+        })
+        
+      }else{
+        contraseñaIncorrecta = true;
+        res.render("index", { contraseñaIncorrecta, layout: false });
+      }
+    }else{
+      emailIncorrecto = true;
+      res.render("index", { emailIncorrecto, layout: false });
+    }
   }
 });
 
@@ -74,108 +100,121 @@ app.post("/registro", async (req, res) => {
   let pass1 = req.body.password1;
   let pass2 = req.body.passwordConfirmacion;
 
-  const user = new Usuario({ nombre: "Raúl", correo_electronico: "raulsr64@gmail.com", password: "a"})
-  user.save();
-  console.log(user);;
   
-  /*
-  if(arrayUsuario)
-  {
-    console.log("prueb");
-    res.render("registro", { layout: false });
-    
-    
+  
+  if((nombre.length <= 0) || (email.length <= 0) ){
+    datosIncompletos = true;
+    res.render("registro", { datosIncompletos, layout: false });
+  }else if((password == null) || (pass1 != pass2)){
+    contrasenasDiferentes = true;
+    res.render("registro", { contrasenasDiferentes, layout: false });
   }else{
-    console.log("Ya existe");
-    res.render("registro", { layout: false });
-  }
-  */
+    const checkEmail = await Usuario.findOne({ correo_electronico: email});
+    if(!checkEmail){
+      const user = new Usuario({ nombre: nombre, correo_electronico: email, password: pass1})
+      user.save();
+      req.session.email = email;
+        req.session.save(function (err) {
+          res.render("home", { nombre });
+      })
+    }else{
+      emailYaRegistrado = true;
+      res.render("registro", { emailYaRegistrado, layout: false });
+    }
+  } 
 });
 
 
 // Pagina Home
 app.get("/home", (req, res, next) => {
   
-  //if (!req.body.email) {
-   // res.redirect("/");
-    //poner que se muestre mensaje de inicio incorrecto (Sporify)
-  //} else {
-
-    //inicio correcto
-    //let email = req.body.email;
-
+  if (!req.session.email) {
+    res.redirect("/");
+  }else{
     res.render("home");
-  //}
+  }
 });
 
 
 // Página Esperanza De Vida
 app.get("/esperanzaVida", (req, res, next) => {
 
-  let data = [
-    {
-      imagen: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f2/Americas_on_the_globe_%28red%29.svg/1200px-Americas_on_the_globe_%28red%29.svg.png",
-      datos: {
-        name: "América",
-        global: "79",
-        hombres: "77",
-        mujeres: "81",
+  if (!req.session.email) {
+    res.redirect("/");
+  }else{
+    let data = [
+      {
+        imagen: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f2/Americas_on_the_globe_%28red%29.svg/1200px-Americas_on_the_globe_%28red%29.svg.png",
+        datos: {
+          name: "América",
+          global: "79",
+          hombres: "77",
+          mujeres: "81",
+        },
       },
-    },
-    {
-      imagen: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a0/Europe_on_the_globe_%28red%29.svg/1200px-Europe_on_the_globe_%28red%29.svg.png",
-      datos: {
-        name: "Europa",
-        global: "78",
-        hombres: "74",
-        mujeres: "81",
+      {
+        imagen: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a0/Europe_on_the_globe_%28red%29.svg/1200px-Europe_on_the_globe_%28red%29.svg.png",
+        datos: {
+          name: "Europa",
+          global: "78",
+          hombres: "74",
+          mujeres: "81",
+        },
       },
-    },
-    {
-      imagen: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/11/Oceania_on_the_globe_%28red%29_%28Polynesia_centered%29.svg/1200px-Oceania_on_the_globe_%28red%29_%28Polynesia_centered%29.svg.png",
-      datos: {
-        name: "Oceanía",
-        global: "77",
-        hombres: "75",
-        mujeres: "80",
+      {
+        imagen: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/11/Oceania_on_the_globe_%28red%29_%28Polynesia_centered%29.svg/1200px-Oceania_on_the_globe_%28red%29_%28Polynesia_centered%29.svg.png",
+        datos: {
+          name: "Oceanía",
+          global: "77",
+          hombres: "75",
+          mujeres: "80",
+        },
       },
-    },
-    {
-      imagen: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/24/Asia_on_the_globe_%28red%29.svg/1200px-Asia_on_the_globe_%28red%29.svg.png",
-      datos: {
-        name: "Asia",
-        global: "72",
-        hombres: "70",
-        mujeres: "74",
+      {
+        imagen: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/24/Asia_on_the_globe_%28red%29.svg/1200px-Asia_on_the_globe_%28red%29.svg.png",
+        datos: {
+          name: "Asia",
+          global: "72",
+          hombres: "70",
+          mujeres: "74",
+        },
       },
-    },
-    {
-      imagen: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a4/Africa_on_the_globe_%28red%29.svg/1200px-Africa_on_the_globe_%28red%29.svg.png",
-      datos: {
-        name: "África",
-        global: "60",
-        hombres: "58",
-        mujeres: "61",
+      {
+        imagen: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a4/Africa_on_the_globe_%28red%29.svg/1200px-Africa_on_the_globe_%28red%29.svg.png",
+        datos: {
+          name: "África",
+          global: "60",
+          hombres: "58",
+          mujeres: "61",
+        },
       },
-    },
-  ];
-
-  res.render("esperanzaVida", {
-    data: data,
-    esperanzaVidaCard: req.query.data,
-  });
+    ];
+  
+    res.render("esperanzaVida", {
+      data: data,
+      esperanzaVidaCard: req.query.data,
+    });
+  }
 });
 
 
 // Página Natalidad
 app.get("/natalidad", (req, res, next) => {
-  res.render("natalidad");
+  if (!req.session.email) {
+    res.redirect("/");
+  }else{
+    res.render("natalidad");
+  }
 });
 
 
 // Página Contacto
 app.get("/contacto", (req, res, next) => {
-  res.render("contacto");
+  if (!req.session.email) {
+    res.redirect("/");
+  }else{
+    res.render("contacto");
+  }
 });
 
 // Enviar correo con los datos del formulario de contacto
